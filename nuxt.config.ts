163500f432982dc2291174667a48f864dc29f9c6
Nam Nguyen Hai <doluadao1995@gmail.com>
2025-10-06 20:58:15 +0200

    feat: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    'nuxt-lightningcss',
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  vite: { 
+    plugins: [
+      tailwindcss()
+    ]
+  }
 })
